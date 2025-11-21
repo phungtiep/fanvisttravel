0a81a90f3940352c194d32b5ae38f7898cfe70cd
Sỹ Minh Phụng Tiếp <syminhphungtiep@Sys-MacBook-Pro.local>
2025-11-21 15:17:59 +0700

    update speed insight

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -30,6 +32,7 @@ export default function App() {
       </Routes>
       <FloatingButtons />
       <Footer />
+      <SpeedInsights/>
     </BrowserRouter>
   );
 }
