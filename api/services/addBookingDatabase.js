b02185038e685fcb14754cb5929e7c77cc863278
Tiếp Sỹ Minh Phụng <43229272+phungtiep@users.noreply.github.com>
2025-12-01 11:37:07 +0700

    update new booking to database 3

--- api/services/addBookingDatabase.js
+++ api/services/addBookingDatabase.js
@@ -1,30 +1,36 @@
 import { supabase } from "../lib/superbase.js";
 
-
 export async function addBookingToDatabase(data) {
   const payload = {
     full_name: data.fullName,
     phone: data.phone,
     email: data.email,
+
     route: data.route,
     car_type: data.carType,
-    pickup: data.pickupPlace,
-    dropoff: data.dropoffPlace,
-    adult: data.adultCount,
-    child: data.childCount,
+
+    pickup_place: data.pickupPlace,
+    dropoff_place: data.dropoffPlace,
+
     date: data.date,
     time: data.time,
-    roundtrip: data.roundTrip,
-    return_date: data.returnDate,
-    return_time: data.returnTime,
-    total_price: data.totalPrice,
+
+    round_trip: data.roundTrip,
+    return_date: data.returnDate || null,
+    return_time: data.returnTime || null,
+
     note: data.note,
+
+    adult_count: data.adultCount,
+    child_count: data.childCount,
+
+    total_price: data.totalPrice,
   };
 
   const { error } = await supabase.from("bookings").insert(payload);
 
   if (error) {
-    console.error("Error inserting booking:", error.message);
+    console.error("🚨 Lỗi khi lưu booking:", error.message);
     throw error;
   }
 
